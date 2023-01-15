d5f9f2a765faf214fc62ff037e8ac8d4210cc52d
SH0100BOY <80843835+Hordii4uk@users.noreply.github.com>
2023-01-15 14:19:16 +0200

    Update js.js

--- js.js
+++ js.js
@@ -40,6 +40,7 @@ window.addEventListener('scroll',(event) => {
   console.log(Math.max(document.body.scrollTop, document.documentElement.scrollTop))
 });
 
+
 // check scroll up or down
 window.onscroll = function(e) {
 // print "false" if direction is down and "true" if up
@@ -49,10 +50,11 @@ this.oldScroll = this.scrollY;
 
 
 // addEventListener for more 1 elem:
+// 1
 [qty_input, qty_decrease].map(elem => elem.addEventListener('click', e=>{
   console.log('your cliked on', elem);
 }))
-
+// 2
 (function listenItems() {
   const els = Array.prototype.slice.call(document.querySelectorAll('input[data-additional-item]'));
   els.forEach(function (el) { el.addEventListener('change', testCheck); });
@@ -79,3 +81,23 @@ const showPosts = async () => {
 console.log(posts);
 }
 showPosts();
+
+
+// Get the unique values from arrays
+const numbers = [ 1, 1, 2, 3, 4, 4, 5]
+console.log([ ...new Set(numbers)]) // [1, 2, 3, 4, 5]
+
+// JS this
+var obj = {
+    name: "shoaib",
+    func: function() {
+        var self = this;
+        console.log("outer function:  this.name = " + this.name); // outer function:  this.name = shoaib
+        console.log("outer function:  self.name = " + self.name); // outer function:  self.name = shoaib
+        (function() {
+            console.log("inner function:  this.name = " + this.name); // inner function:  this.name = undefined (the reference to the local variable self remains in scope and has proper access)
+            console.log("inner function:  self.name = " + self.name); // inner function:  self.name = shoaib
+        }());
+    }
+};
+obj.func();
